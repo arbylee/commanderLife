f557cbcaed2c702b173f3c0773a3b316d2280273
Richard Lee <richard.lee@steelseries.com>
2024-08-09 09:55:29 -0500

    Initial Commit

---
+++ life/lifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifeApp.swift
+//  life
+//
+//  Created by Richard on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct lifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
