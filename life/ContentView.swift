174b557bb14bc595075437149e4c4e33565a0248
Richard Lee <richard.lee@steelseries.com>
2024-08-10 16:14:00 -0500

    wip

--- life/ContentView.swift
+++ life/ContentView.swift
@@ -10,12 +10,56 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                HStack {
+                    ZStack {
+                        Color(.red)
+                        HStack{
+                            Button(action: greeting, label: {
+                                Text("")
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+                            })
+                            .buttonStyle(.borderedProminent)
+//                            .padding()
+//                            .rotationEffect(Angle(degrees: 0))
+                            Button(action: greeting, label: {
+                                Text("")
+                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            })
+                            .buttonStyle(.borderedProminent)
+//                            .rotationEffect(Angle(degrees: 0))
+//                            .padding()
+                        }
+                    }
+                }
+                HStack {
+                    ZStack {
+                        Color(.green)
+                    }
+                }
+            }
+            HStack {
+                HStack {
+                    ZStack {
+                        Color(.blue)
+                    }
+                }
+                HStack {
+                    ZStack {
+                        Color(.yellow)
+                    }
+                }
+            }
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
         }
         .padding()
+    }
+    func greeting() {
+        print("Hello, World!")
     }
 }
 
